78a8bf675498ff0c06727ac8365eddcc65efba07
Xnaizer <seykig028@gmail.com>
2026-05-08 23:59:58 +0700

    (create: learn stuff folder): initiate notes for learnings

---
+++ Learn Stuff/typescripts.ts
@@ -0,0 +1,35 @@
+type TUser = {
+    name: string;
+    umur: number;
+    createdAt: Date;
+
+}
+
+type TStatusVehicle = "New" | "Used" | "Broken"
+
+interface IVehicle {
+    id: string | number;
+    name: string;
+    description?: string;
+    isFavorite: boolean;
+    status: TStatusVehicle;
+    photoURL?: "LINK"
+}
+
+
+interface ILambo extends IVehicle {
+    message: string
+}
+
+const dataLambo: ILambo = {
+    id: 1,
+    name: 'supra',
+    isFavorite: false,
+    status: "New",
+    message: "Hello teman"
+}
+
+interface IVehicleToken extends Omit<IVehicle, "id" > {
+    id: Object
+}
+
