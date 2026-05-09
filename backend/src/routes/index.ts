7a8c2f6cfdf73e266784eacb59c118445538169a
Alfaraby <alfrby09@gmail.com>
2026-05-10 03:14:39 +0700

    (feat: routes) creating route for odi

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,5 +4,6 @@ import dummyController from "../controllers/dummy.controller"
 const routes = express.Router()
 
 routes.get("/dummy", dummyController.dummy)
+routes.get("/odi", dummyController.odi)
 
 export default routes
