268a6c776e2da7ed918fa694bfa6339ccc2b5a90
Alfaraby <alfrby09@gmail.com>
2026-05-10 23:02:16 +0700

    (feat: route): intializing authRoute and post get from auth.controller

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,13 @@
 import express from 'express'
+import authController from '../controllers/auth.controller';
 
 const authRoute = express.Router()
 
-// authRoute.get("/login",)
+authRoute.post("/login", authController.login) // post
+authRoute.post("/register", authController.register) // post
+authRoute.get("/me", authController.me) // get
+authRoute.post("/reset_password", authController.reset_password) // post
+authRoute.post("/asking_reset_password", authController.asking_reset_password) // post
 
 
 export default authRoute;
