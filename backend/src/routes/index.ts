734b851cc2cb6a4b9af6293a40e13d754675b965
Xnaizer <seykig028@gmail.com>
2026-05-10 22:09:24 +0700

    (feat: index route): imeplement auth route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import express  from "express"
 import dummyController from "../controllers/dummy.controller"
+import authRoute from "./auth.route"
 
 const routes = express.Router()
+
+routes.use("/auth", authRoute)
 
 routes.get("/dummy", dummyController.dummy)
 routes.get("/odi", dummyController.odi)
