481dccb41ce59fa91c138b04d2710eb2f58e838a
Alfaraby <alfrby09@gmail.com>
2026-05-11 02:17:22 +0700

    (feat: control): creating validation for login and register

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,17 +1,96 @@
 import { Request, Response } from "express";
+import UserModel from "../models/user.model";
+import BcryptVal from "../utils/bcrypt";
+
+type TLogin= {
+    identifier: string;
+    password: string;
+}
+
+type TRegister= {
+    name: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
 
 export default {
-    login(req: Request, res: Response) {
-        return res.status(200).json({
-            message:"Login",
-            data:"null"
-        })
+    async login(req: Request, res: Response) {
+        const {identifier, password} = req.body as unknown as TLogin
+        try{
+            const userByIdentifier = await UserModel.findOne({
+                $or: [ 
+                    {
+                        email: identifier
+                    },
+                    {
+                        username: identifier
+                    }
+                ]
+            })
+
+            if(!userByIdentifier){
+                return res.status(404).json({
+                    message:"User Not Found",
+                    data: null
+                })
+            }
+            if(!userByIdentifier.is_active){
+                return res.status(400).json({
+                    message:"Please Activated your Account",
+                    data: null
+                })
+            }
+            
+            const validPassword = await BcryptVal.comparePassword(password, userByIdentifier.password)
+            
+            if(!validPassword){
+                return res.status(400).json({
+                    message:"Password Incorrect",
+                    data: null
+                })
+            }
+
+            return res.status(200).json({
+                message:`Welcome ${userByIdentifier.name}`,
+                data: userByIdentifier
+            })
+
+        }catch(error){
+            return res.status(500).json({
+                message:"Terjadi Kesalahan Pada Server",
+                data: error
+            })
+        }
     },
-    register(req: Request, res: Response) {
-        return res.status(200).json({
-            message:"register",
-            data:"null"
-        })
+    async register(req: Request, res: Response) {
+        
+        try{
+            const createUser = await UserModel.create(req.body)
+
+            if(!createUser){
+                return res.status(400).json({
+                    message:"Please input correctly",
+                    data: null
+                })
+            }
+
+            if(createUser){
+                return res.status(200).json({
+                    message:`Registrasi Berhasil hai ${createUser.name}` ,
+                    data:createUser
+                })
+            }
+
+
+
+        }catch(error){
+            return res.status(500).json({
+                message:"Terjadi Kesalahan Pada Server",
+                data: error
+            })
+        }
     },
     me(req: Request, res: Response) {
         return res.status(200).json({
